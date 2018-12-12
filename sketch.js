f2f57f902003780e55f35aa2b5c55ba9f7af8380
Scott Plunkett <splunkett@8thLight.com>
2018-12-12 17:31:00 -0600

    [PARTICLES]

--- sketch.js
+++ sketch.js
@@ -1,54 +1,138 @@
-var mic;
-var capture;
-
-function audioSetup() {
-  // Create an Audio input
-  mic = new p5.AudioIn();
-
-  // start the Audio Input.
-  // By default, it does not .connect() (to the computer speakers)
-  mic.start();
-  mic.connect();
-}
-
-
-
-function videoSetup() {
-  capture = createCapture(VIDEO);
-  capture.size(320, 240);
-  capture.hide();
-}
-
-function drawVideo() {
-  
-}
+var systems;
 
 function setup() {
-  var audCanvas = createCanvas(1000, 1000);
-  audCanvas.parent('audio-container');
-  audioSetup();
-  videoSetup();
+  createCanvas(windowWidth-10, windowHeight-10);
+  systems = [];
 }
 
 function draw() {
-  drawVideo();
-
-
-  // Get the overall volume (between 0 and 1.0)
-  var vol = mic.getLevel();
-  fill(127);
-  stroke(0);
-  // Draw an ellipse with height based on volume
-  var h = map(vol, 0, 1, height, 0);
-  background(255);
-  image(capture, width/4, h - 500, 500, 500);
-  filter('INVERT');
+  background(systems.length + 100, systems.length, 0);
+  background(systems.length*Math.random()*100,systems.length*Math.random()*100, systems.length*Math.random()*100);
+  for (i = 0; i < systems.length; i++) {
+    fill(i+50, i, 100, .8);
+    systems[i].run();
+    systems[i].addParticle();
+  }
+  if (systems.length==0) {
+    fill(255, 200, 100, .8);
+    textAlign(CENTER);
+    textSize(32);
+    text("click mouse to add particle systems", width/2, height/2);
+  }
 }
 
-setup();
-draw();
+function mousePressed() {
+  this.p = new ParticleSystem(createVector(mouseX, mouseY));
+  systems.push(p);
+}
 
+// A simple Particle class
+var Particle = function(position) {
+  this.acceleration = createVector(0, 0.05);
+  this.velocity = createVector(random(-1, 1), random(-1, 0));
+  this.position = position.copy();
+  this.lifespan = 255.0;
+};
 
+Particle.prototype.run = function() {
+  this.update();
+  this.display();
+};
 
+// Method to update position
+Particle.prototype.update = function(){
+  this.velocity.add(this.acceleration);
+  this.position.add(this.velocity);
+  this.lifespan -= 2;
+};
 
+// Method to display
+Particle.prototype.display = function () {
+  stroke(200, this.lifespan);
+  strokeWeight(2);
+  fill(127, this.lifespan);
+  ellipse(this.position.x, this.position.y, 12, 12);
+};
 
+// Is the particle still useful?
+Particle.prototype.isDead = function () {
+  if (this.lifespan < 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+var ParticleSystem = function (position) {
+  this.origin = position.copy();
+  this.particles = [];
+};
+
+ParticleSystem.prototype.addParticle = function () {
+  // Add either a Particle or CrazyParticle to the system
+  if (int(random(0, 2)) == 0) {
+    p = new Particle(this.origin);
+  }
+  else {
+    p = new CrazyParticle(this.origin);
+  }
+  this.particles.push(p);
+};
+
+ParticleSystem.prototype.run = function () {
+  for (var i = this.particles.length - 1; i >= 0; i--) {
+    var p = this.particles[i];
+    p.run();
+    if (p.isDead()) {
+      this.particles.splice(i, 1);
+    }
+  }
+};
+
+// A subclass of Particle
+
+function CrazyParticle(origin) {
+  // Call the parent constructor, making sure (using Function#call)
+  // that "this" is set correctly during the call
+  Particle.call(this, origin);
+
+  // Initialize our added properties
+  this.theta = 0.0;
+};
+
+// Create a Crazy.prototype object that inherits from Particle.prototype.
+// Note: A common error here is to use "new Particle()" to create the
+// Crazy.prototype. That's incorrect for several reasons, not least 
+// that we don't have anything to give Particle for the "origin" 
+// argument. The correct place to call Particle is above, where we call 
+// it from Crazy.
+CrazyParticle.prototype = Object.create(Particle.prototype); // See note below
+
+// Set the "constructor" property to refer to CrazyParticle
+CrazyParticle.prototype.constructor = CrazyParticle;
+
+// Notice we don't have the method run() here; it is inherited from Particle
+
+// This update() method overrides the parent class update() method
+CrazyParticle.prototype.update=function() {
+  Particle.prototype.update.call(this);
+  // Increment rotation based on horizontal velocity
+  this.theta += (this.velocity.x * this.velocity.mag()) / 10.0;
+}
+
+// This display() method overrides the parent class display() method
+CrazyParticle.prototype.display=function() {
+  // Render the ellipse just like in a regular particle
+  Particle.prototype.display.call(this);
+  // Then add a rotating line
+  push();
+  translate(this.position.x,this.position.y)
+  translate(this.position.y, this.velocity.x)
+  translate(this.position.y, this.position.x)
+  rotate(this.theta);
+  rotate(this.theta*this.theta);
+  stroke(this.position.x,this.lifespan);
+  color(255,this.lifespan,0)
+  line(0,0,25,0);
+  pop();
+}
